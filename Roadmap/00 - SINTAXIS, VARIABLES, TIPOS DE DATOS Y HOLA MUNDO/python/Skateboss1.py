3178ac8bc19d95e0b2ca89fff35b382fa0de8127
Skateboss1 <brayangalvis09@gmail.com>
2025-06-12 18:10:48 -0500

    Skateboss1.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Skateboss1.py
@@ -0,0 +1,29 @@
+https://www.python.org/ 
+
+# numeral se utiliza para hacer comentarios 
+
+""" 
+texto entre comillas dobles para hacer comentarios tambien 
+"""
+
+'''
+o en comillas simples tambien 
+'''
+
+# las variables van en minusculas y con guiones bajos por cada nueva palabra
+
+my_variable = "mi variable"
+
+MY_CONSTANT = "mi constante" 
+
+my_int = 1  # para numeros enteros 
+my_float = 1.5  # para numero decimales 
+my_bool = True 
+my_bool = False
+my_string = "cadena de texto"
+
+# datos primitivos 
+
+para imprimir texto 
+
+print ("hola python")
